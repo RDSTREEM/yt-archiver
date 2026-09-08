743d322c486043fbd6f98c6c39d1edb7cfa3a7a3
RDSTREEM <nahomabebe2002@gmail.com>
2026-09-08 11:22:32 +0000

    started on the chunking?

--- src/chunker.py
+++ src/chunker.py
@@ -4,9 +4,20 @@ from config import CHUNK_SIZE
 
 def chunk(file_paths: list):
     chunks = []
+    m = {}
     for path in file_paths:
         with open(path, "rb+") as file:
-            b = file.read(CHUNK_SIZE)
+            size = len(file.read())
+            file.seek(0)
+            while file.tell() < size:
+                if chunks:
+                    prev_chunk = chunks[-1]
+                    rem = CHUNK_SIZE - len(prev_chunk)
+                    b = file.read(rem)
+                    if b:
+                        prev_chunk = (prev_chunk << rem) + b
+                else:
+                    chunks.append(file.read)
 
     print(chunks)
-    return chunks
+    return chunks, m
