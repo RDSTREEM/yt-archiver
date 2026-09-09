931af12834f9903ef55813b90dceb50c7ddd48d9
RDSTREEM <nahomabebe2002@gmail.com>
2026-09-09 05:52:57 +0000

    fixed the way chunking works

--- src/chunker.py
+++ src/chunker.py
@@ -3,21 +3,21 @@ from config import CHUNK_SIZE
 
 
 def chunk(file_paths: list):
-    chunks = []
+    chunks: list[bytes] = []
     m = {}
     for path in file_paths:
         with open(path, "rb+") as file:
-            size = len(file.read())
-            file.seek(0)
-            while file.tell() < size:
-                if chunks:
-                    prev_chunk = chunks[-1]
-                    rem = CHUNK_SIZE - len(prev_chunk)
-                    b = file.read(rem)
-                    if b:
-                        prev_chunk = (prev_chunk << rem) + b
+            data = file.read()
+            prev_chunk = b"" if not chunks else chunks[-1]
+            # i = 0
+            while len(data) > 0:
+                rem = CHUNK_SIZE - len(prev_chunk)
+                current = data[rem:]
+                if rem > 0:
+                    chunks[-1] = (prev_chunk << rem) + current
                 else:
-                    chunks.append(file.read)
+                    chunk.append(current)
+                data = data[rem:]
 
     print(chunks)
     return chunks, m
