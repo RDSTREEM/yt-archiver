e63836bf9fb8a872c1d7cf8191b6a378c35ea05a
RDSTREEM <nahomabebe2002@gmail.com>
2026-09-13 10:44:55 +0000

    made it so that chunk_bytes doesnt know anything abt file types and stuff, need to make it into a streaming implementation for the future tho

--- src/chunker.py
+++ src/chunker.py
@@ -1,26 +1,9 @@
 import os
-from config import CHUNK_SIZE
 
 
-def chunk(file_paths: list):
-    chunks: list[bytes] = []
-    file_map: dict[str, tuple[int, int]] = {}
-    for path in file_paths:
-        with open(path, "rb") as file:
-            data = file.read()
-            prev_chunk = b"" if not chunks else chunks[-1]
-            # i = 0
-            start = len(chunks)
-            while len(data) > 0:
-                rem = CHUNK_SIZE - len(prev_chunk)
-                current = data[rem:]
-                if rem > 0:
-                    chunks[-1] = prev_chunk + current
-                else:
-                    chunks.append(current)
-                data = data[rem:]
-            end = len(chunks)
-            file_map[path] = (start, end)
+def chunk_bytes(data: list, chunk_size):
+    if chunk_size <= 0:
+        raise ValueError("Chunk size must be a positive value")
 
-    print(chunks)
-    return chunks, file_map
+    for start in range(0, len(data), chunk_size):
+        yield data[start : start + chunk_size]
