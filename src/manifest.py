7e2ad8d945553d8b7103cf6281211c4fd501638e
RDSTREEM <nahomabebe2002@gmail.com>
2026-09-17 14:15:33 +0000

    weird way to make manifest

--- src/manifest.py
+++ src/manifest.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from config import CHUNK_SIZE
 
 
 @dataclass
@@ -14,3 +15,42 @@ class FileInfo:
     chunks: list[
         str
     ]  # may change to list[ChunkInfo] if i decide to make each chunk size different
+
+
+"""
+Manifest Format
+
+
+manifest.json
+
+version: ...
+c_size: ...
+files: {
+    x: [start, end]
+}
+
+
+"""
+
+
+class Manifest:
+    def __init__(self, files_path) -> None:
+        data: bytes = b""
+        chunk = 0
+        file_map = {}
+        for path in files_path:
+            start = chunk
+            with open(path, mode="rb+") as file:
+                data += file.read()
+                index = 0
+                while index < len(data):
+                    chunk += 1
+                    index += CHUNK_SIZE
+            file_map[path] = (start, chunk + 1)
+
+        self.data = data
+        self.num_chunks = chunk
+        self.file_map = file_map
+
+    def generate_manifest(self):
+        pass
